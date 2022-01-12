b2afaa430698a397937457a2067339611ce54f2c
Danny <danny613tran@gmail.com>
2022-01-11 20:31:35 -0500

    Completed combining

--- tempSensor.py
+++ tempSensor.py
@@ -1,3 +1,4 @@
+# Import statements
 import os
 import glob
 import time
@@ -7,21 +8,38 @@ import RPi.GPIO as GPIO
 
 # General Setup
 pins = [16]
-GPIO.setmode(GPIO.BCM) #sets how we reference GPIO pins
-GPIO.setup(pins, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) #set GPIO pins
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(pins, GPIO.OUT)
 
-os.system('modprobe w1-gpio')
-os.system('modprobe w1-therm')
+# os.system('modprobe w1-gpio')
+# os.system('modprobe w1-therm')
 
 base_dir = '/sys/bus/w1/devices/'
 device_path = glob.glob(base_dir + '28*')[0] # Get file path of sensor
 
 # Function definitions
-def read_temp_raw():
-    with open(device_path +'/w1_slave','r') as f:
-        print(f)
+def ledON():
+    GPIO.output(pins[0], GPIO.HIGH)
+
+def ledOFF():
+    GPIO.output(pins[0], GPIO.LOW)
+
+try:
+    while True:
+        with open(device_path + '/w1_slave', 'r') as f:
+            temp = int((str(f.readlines()))[76:81])/1000
+        print(temp)
+        if temp >= 20 and temp <= 25:
+            ledON()
+            print("READY")
+        elif GPIO.input(pins[0]) == 1:
+            ledOFF()
+        time.sleep(1)
+
+except:
+    GPIO.cleanup()
         # valid, temp = f.readlines()
-read_temp_raw()
+
 #     return valid, temp
  
 # def read_temp():
