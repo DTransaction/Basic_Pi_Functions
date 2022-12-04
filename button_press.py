2fb45af3a18b0ff679fe29319f5c41cb9939809b
DTransaction <danny613tran@gmail.com>
2022-12-04 00:16:17 -0500

    Testing GPIO event_detect

--- button_press.py
+++ button_press.py
@@ -2,18 +2,18 @@ import time
 import RPi.GPIO as GPIO
 
 #General Setup
-def button_callback(channel):
+def button_callback(count):
     print(count)
     count += 1
 
 GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
 pin = [8]
+count = 0
 
 GPIO.setwarnings(False)
 GPIO.setup(pin[0], GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set to be an input pin and set initial value to be pulled low (off)
-GPIO.add_event_detect(pin[0],GPIO.RISING,callback=button_callback) # Setup event on pin rising edge
+GPIO.add_event_detect(pin[0],GPIO.RISING,callback=button_callback(count)) # Setup event on pin rising edge
 
-count = 0
 message = input("Press enter to quit\n\n") # Run until someone presses enter
 
 
