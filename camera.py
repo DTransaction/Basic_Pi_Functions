eae534054969bbdeb283e6e239727f36fb3544f9
DTransaction <danny613tran@gmail.com>
2022-12-23 10:31:42 -0500

    Testing camera code

--- camera.py
+++ camera.py
@@ -5,5 +5,5 @@ camera = PiCamera()
 
 camera.start_preview()
 time.sleep(5)
-camera.capture('/home/dannypi/PROOF/' + time.ctime() + ".jpg")
+camera.capture('/home/dannypi/PROOF/image.jpg')
 camera.stop_preview()
